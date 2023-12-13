be78b47ef0f609ae2b9e0494047be91813fcead2
Varun <sharmavarun.1912@gmail.com>
2023-12-14 00:36:50 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <div>
+          <div className="text-white bg-black">hello</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
