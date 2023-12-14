e100b1f7dc4053b18a3529dba6829ffa43ee3b05
Varun <sharmavarun.1912@gmail.com>
2023-12-14 15:04:19 +0530

    created a test api route

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 dotenv.config();
 
+//---------------------------------------------------------------------------------------
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
@@ -13,7 +15,14 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+//---------------------------------------------------------------------------------------
 
+//---------------------------------------------------------------------------------------
+app.use("/api/user", userRoutes);
+//---------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
+//---------------------------------------------------------------------------------------
