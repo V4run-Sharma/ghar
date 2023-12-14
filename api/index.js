5157ba9df7a6f147e6677ab488d1dd45269165a3
Varun <sharmavarun.1912@gmail.com>
2023-12-14 14:38:29 +0530

    connecting to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
