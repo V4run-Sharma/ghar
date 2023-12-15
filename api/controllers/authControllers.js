d87fc00db582dfdd23f542fe442845d250f622c7
Varun <sharmavarun.1912@gmail.com>
2023-12-15 20:47:17 +0530

    created middleware and custom error handler func

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,12 +1,14 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
-  await User.create({ username, email, password: hashedPassword });
-  res.status(201).json({
-    message: "User created successfully",
-  });
+  try {
+    await User.create({ username, email, password: hashedPassword });
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
 };
