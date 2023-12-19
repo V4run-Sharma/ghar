eabfd6fceb1d4bdcd9f6a80476b9777c81d551a1
Varun <sharmavarun.1912@gmail.com>
2023-12-19 22:45:04 +0530

    Fixed signup issues

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -4,9 +4,19 @@ import bcrypt from "bcrypt";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
   try {
-    await newUser.save();
+    const existingEmail = await User.findOne({ email });
+    const existingUsername = await User.findOne({ username });
+    if (existingEmail) {
+      throw new Error("Email already exists");
+    }
+    if (existingUsername) {
+      throw new Error("Username already exists");
+    }
+
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     next(error);
