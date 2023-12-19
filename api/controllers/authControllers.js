1caa734891f65a1033d0286dc5c5635902f77d0e
Varun <sharmavarun.1912@gmail.com>
2023-12-20 00:28:55 +0530

    Created Sign In api route

--- api/controllers/authControllers.js
+++ api/controllers/authControllers.js
@@ -1,5 +1,6 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -24,13 +25,18 @@ export const signup = async (req, res, next) => {
 };
 
 export const signin = async (req, res, next) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
     if (!validUser) throw new Error("User Not Found");
 
     const validPassword = bcrypt.compareSync(password, validUser.password);
     if (!validPassword) throw new Error("Invalid credentials");
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pw, ...rest } = validUser._doc; // This is to prevent password from getting leaked
+
+    res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/authControllers.js";
+import { signin, signup } from "../controllers/authControllers.js";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
+router.post("/sign-in", signin);
 
 export default router;
