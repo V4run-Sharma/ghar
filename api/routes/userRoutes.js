6f40e3a5bcb740980105a96afca509d2e9bf76e7
Varun <sharmavarun.1912@gmail.com>
2024-02-20 14:47:32 +0530

    Added "Get All User Listings" route

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   test,
   updateUser,
   deleteUser,
+  getUserListing,
 } from "../controllers/userControllers.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.patch("/update/:id", verifyUser, updateUser);
 router.delete("/delete/:id", verifyUser, deleteUser);
+router.get("/listings/:id", verifyUser, getUserListing);
 
 export default router;
